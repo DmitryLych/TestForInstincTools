9189e8a9992cf2727af41d0bbbf66888ab997901
Dmitry Lych <dmitrylych@gmail.com>
2017-09-19 22:05:09 +0300

    delete .idea

--- src/main/java/org/lych/Main.java
+++ src/main/java/org/lych/Main.java
@@ -4,10 +4,12 @@ package org.lych;
 import org.lych.usersinterface.UsersInterface;
 
 import java.io.IOException;
+import java.io.PrintStream;
 
 public class Main {
     public static void main(String []agrs) throws InterruptedException {
         UsersInterface start=new UsersInterface();
+
         try {
             start.startProject();
         } catch (IOException e) {
